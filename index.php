e95b19034574d4c255bcf17c14839a134fc5f449
FajarTester <fajartester5@gmail.com>
2025-10-27 23:04:41 +0700

    Menambahkan title

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-    $list_name = array("Fajar", "Agir", "Cantarella");
-    $list_name_string = implode(", ", $list_name);
+        $list_name = array("Fajar", "Agir", "Cantarella");
+        $list_name_string = implode(", ", $list_name);
+        $title = "Mantap"
 ?>
 
 
@@ -10,6 +11,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
 </head>
+<body>
+        <h1><?php echo $title;?></h1>
         <h1>Hello, World! <?php echo $list_name_string; ?></h1>
 </body>
-</html>
+</html> 
